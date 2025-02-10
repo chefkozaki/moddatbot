4f1db5d2720395e5da48248a7f88965b406b3d4c
Kozaki <122468057+chefkozaki@users.noreply.github.com>
2025-02-10 19:48:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,23 +15,9 @@ const client = new Client({
   });
 let userInventory = {};
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 const pathinv = './inventory.json'; // Đường dẫn file lưu kho đồ
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
 
 
 client.once('ready', () => {
